077cae841de0cc49ef93bfe7c9d8e59b56600d16
Nemanja Zbiljić <nemanja.zbiljic@gmail.com>
2016-08-13 07:59:04 +0200

    Initial import

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
 # mvns
+
+CLI tool to search artifacts in maven central repository.
+
+Install
+-------
+
+    $ go get github.com/zbiljic/mvns
+
+Update
+------
+
+When new code is merged to master, you can use
+
+    $ go get -u github.com/zbiljic/mvns
+
+To retrieve the latest version of `mvns`.
+
+Usage
+-----
+
+### Search by query
+
+```sh
+> mvns netty
+compile 'com.orange.redis-protocol:netty:0.12'
+compile 'org.jboss.netty:netty:3.2.10.Final'
+compile 'com.github.spullara.redis:netty:0.7'
+compile 'io.netty:netty:4.0.0.Alpha8'
+compile 'org.jboss.errai.io.netty:netty:4.0.0.Alpha1.errai.r1'
+compile 'io.netty:netty-parent:5.0.0.Alpha2'
+...
+```
+
+### Search by groupId, artifactId and version
+
+- `-g` Specify groupId.
+- `-a` Specify artifactId.
+- `-v` Specify version.
+
+```sh
+mvns -g io.netty -a netty-all -v 4.1.4.Final
+compile 'io.netty:netty-all:4.1.4.Final'
+```
+
+### Select all versions
+
+- `-A` Show all versions.
+
+```sh
+mvns -g io.netty -a netty-all -A
+compile 'io.netty:netty-all:4.1.4.Final'
+compile 'io.netty:netty-all:4.0.40.Final'
+compile 'io.netty:netty-all:4.1.3.Final'
+compile 'io.netty:netty-all:4.0.39.Final'
+compile 'io.netty:netty-all:4.1.2.Final'
+compile 'io.netty:netty-all:4.0.38.Final'
+compile 'io.netty:netty-all:4.0.37.Final'
+compile 'io.netty:netty-all:4.1.1.Final'
+compile 'io.netty:netty-all:4.1.0.Final'
+...
+```
+
+### Limit number of result.
+
+- `-m` Limit number of result. Default is 20.
+
+```sh
+mvns -m 1 io.netty
+compile 'io.netty:netty-parent:5.0.0.Alpha2'
+```
+
+License
+-------
+This project is made available under the [Apache 2.0 License](http://www.apache.org/licenses/LICENSE-2.0).
+
+---
+
+Copyright © 2016 Nemanja Zbiljić
---
+++ main.go
@@ -0,0 +1,204 @@
+// Copyright 2016 Nemanja Zbiljic
+//
+
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	description = "CLI tool to search artifacts in maven central repository"
+	version     = "0.1.0"
+)
+
+var (
+	fGroupId     = flag.String("g", "", "specify groupId")
+	fArtifactId  = flag.String("a", "", "specify artifactId")
+	fVersion     = flag.String("v", "", "specify version")
+	fAllVersions = flag.Bool("A", false, "show all versions")
+	fMax         = flag.Int("m", 20, "limit number of result")
+)
+
+var Usage = func() {
+	fmt.Fprintf(os.Stderr, "NAME:\n   %s - %s\n", os.Args[0], description)
+	fmt.Fprint(os.Stderr, "\n")
+	fmt.Fprintf(os.Stderr, "USAGE:\n   %s [options] [query]\n", os.Args[0])
+	fmt.Fprint(os.Stderr, "\n")
+	fmt.Fprintf(os.Stderr, "VERSION:\n   %s\n", version)
+	fmt.Fprint(os.Stderr, "\n")
+	fmt.Fprintf(os.Stderr, "OPTIONS:\n")
+	flag.PrintDefaults()
+	fmt.Fprint(os.Stderr, "\n")
+}
+
+////////////////////////////////////////////////////////////////////////
+// Helpers
+////////////////////////////////////////////////////////////////////////
+
+// JSONResponse solrsearch response.
+type JSONResponse struct {
+	Response struct {
+		Docs []struct {
+			ID            string
+			LatestVersion string
+		}
+	}
+}
+
+func collect(data JSONResponse) {
+	// print results.
+	for _, d := range data.Response.Docs {
+		var line string
+		if len(d.LatestVersion) == 0 {
+			line = fmt.Sprintf("'%s'", d.ID)
+		} else {
+			line = fmt.Sprintf("'%s:%s'", d.ID, d.LatestVersion)
+		}
+		fmt.Printf("compile %s", color(line))
+	}
+}
+
+func color(s string) string {
+	id := strings.Split(s, ":")
+	return fmt.Sprintf("%s:%s:%s\n", colorGroupId(id[0]), colorArtifactId(id[1]), colorVersion(id[2]))
+}
+
+func colorGroupId(s string) string {
+	return fmt.Sprintf("%s%s%s", "\x1b[32m", s, "\x1b[0m")
+}
+
+func colorArtifactId(s string) string {
+	return fmt.Sprintf("%s%s%s", "\x1b[35m", s, "\x1b[0m")
+}
+
+func colorVersion(s string) string {
+	return fmt.Sprintf("%s%s%s%s", "\x1b[34m", "\x1b[1m", s, "\x1b[0m")
+}
+
+func request(params url.Values) (JSONResponse, error) {
+	endpoint := "http://search.maven.org/solrsearch/select?" + params.Encode()
+	res := JSONResponse{}
+	resp, err := http.Get(endpoint)
+	if err != nil {
+		return res, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return res, errors.New(strconv.Itoa(resp.StatusCode))
+	}
+	// parse JSON with anonymous struct.
+	d := json.NewDecoder(resp.Body)
+	if err := d.Decode(&res); err != nil {
+		return res, err
+	}
+	return res, nil
+}
+
+func formatParams(q string) url.Values {
+
+	query := make([]string, 0)
+
+	if len(q) > 0 {
+		query = append(query, q)
+	}
+
+	groupId := *fGroupId
+	if len(groupId) > 0 {
+		query = append(query, appendQuery(query, "g", groupId))
+	}
+
+	artifactId := *fArtifactId
+	if len(artifactId) > 0 {
+		query = append(query, appendQuery(query, "a", artifactId))
+	}
+
+	version := *fVersion
+	if len(version) > 0 {
+		query = append(query, appendQuery(query, "v", version))
+	}
+
+	params := url.Values{
+		"wt":   []string{"json"},
+		"rows": []string{strconv.Itoa(*fMax)},
+		"q":    []string{strings.Join(query, "")},
+	}
+
+	if *fAllVersions {
+		params["core"] = []string{"gav"}
+	}
+
+	return params
+}
+
+func appendQuery(query []string, key, value string) string {
+	if len(query) == 0 {
+		return fmt.Sprintf("%s:\"%s\"", key, value)
+	}
+	return fmt.Sprintf(" AND %s:\"%s\"", key, value)
+}
+
+func containsNoQueryOptions() bool {
+	if len(*fGroupId) > 0 {
+		return true
+	}
+	if len(*fArtifactId) > 0 {
+		return true
+	}
+	return false
+}
+
+////////////////////////////////////////////////////////////////////////
+// main logic
+////////////////////////////////////////////////////////////////////////
+
+func run(args []string) (err error) {
+
+	// Handles cases when too many arguments passed, and no arguments passed
+	// but also no (then) required options
+	if (len(args) > 2) || (len(args) < 1 && !containsNoQueryOptions()) {
+		err = fmt.Errorf("Usage: %s [options] [query]", os.Args[0])
+		return
+	}
+
+	var query string
+
+	if len(args) < 1 {
+		query = ""
+	} else {
+		query = args[0]
+	}
+
+	params := formatParams(query)
+
+	data, err1 := request(params)
+	if err1 != nil {
+		fmt.Printf("Returns status code %s. Please try it again.\n", err1)
+	}
+
+	collect(data)
+
+	fmt.Println()
+
+	return
+}
+
+func main() {
+	flag.Usage = Usage
+
+	flag.Parse()
+
+	err := run(flag.Args())
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
